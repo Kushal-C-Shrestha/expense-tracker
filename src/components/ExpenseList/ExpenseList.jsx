78c130dc4405e9f3c0ad033c13130451bcd7f2f2
Kushal Chandra Shrestha <kushal.c.shrestha@gmail.com>
2026-04-24 17:40:54 +0545

    feat: Expenses listing created with select, edit and delete using Table atomic component.

---
+++ src/components/ExpenseList/ExpenseList.jsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react"
+
+import ExpenseCard from "../ExpenseCard/ExpenseCard"
+import styles from "./ExpenseList.module.css"
+
+import Table from "../ui/Table/Table"
+import Button from "../ui/Button/Button"
+
+import { Pencil, Trash2 } from "lucide-react"
+import { set } from "zod"
+
+const ExpenseList = ({ expenses, filteredExpenses, isFiltering, setExpenses, setSelectedExpense, selectedExpense, handleOpenModal }) => {
+    const data = isFiltering ? filteredExpenses : expenses
+    const [selectedIds, setSelectedIds] = useState([])
+
+    const [isDisabled, setIsDisabled] = useState(false)
+
+    const handleDelete = (id) => {
+        if (selectedIds.length > 1) {
+            console.log("Delete multiple expenses", selectedIds)
+            setExpenses(prevExpenses => prevExpenses.filter(e => !(selectedIds.includes(e.id))));
+            setSelectedIds([])
+            return
+        }
+        setExpenses(prevExpenses => prevExpenses.filter(e => e.id !== id));
+        setSelectedIds([]);
+    }
+    const handleEdit = (id) => {
+        setSelectedExpense(data.find(e => e.id === id));
+        handleOpenModal();
+    }
+
+    const handleSelect = (id) => {
+        setSelectedIds(prevSelectedIds => {
+            if (prevSelectedIds.includes(id)) {
+                return prevSelectedIds.filter(selectedId => selectedId !== id);
+            } else {
+                return [...prevSelectedIds, id];
+            }
+        });
+    }
+
+
+    useEffect(() => {
+        if (selectedIds.length > 1) {
+            setIsDisabled(true);
+            return;
+        }
+        setIsDisabled(false)
+    }, [selectedIds])
+
+
+
+    const columns = [
+        { key: "select", label: "", width: "40px", render: (row) => <input type="checkbox" onChange={() => handleSelect(row.id)} checked={selectedIds.includes(row.id)} /> },
+        { key: "title", label: "Title", width: "40%" },
+        { key: "category", label: "Category", width: "30%" },
+        { key: "amount", label: "Amount", width: "20%" },
+        {
+            key: "actions", label: "Actions", width: "10%", render: (row) => {
+                return (
+                    <div style={{ display: 'flex', gap: '1rem' }}>
+                        <Button variant="edit" icon={<Pencil size={16} onClick={() => handleEdit(row.id)} />} disabled={isDisabled}>
+                            Edit
+                        </Button>
+                        <Button variant="delete" icon={<Trash2 size={16} onClick={() => handleDelete(row.id)} />} >
+                            Delete
+                        </Button>
+                    </div>
+                )
+            }
+        }
+    ]
+
+
+
+    return (
+        <>
+            <Table columns={columns} data={data} className={styles.table} />
+        </>
+    )
+}
+
+export default ExpenseList
---
+++ src/components/ui/Table/Table.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from './Table.module.css'
+
+
+const Table = ({ columns, data }) => {
+    return (
+        <table className={styles.table}>
+            <thead className={styles["table__header"]}>
+                <tr className={styles["table__row"]}>
+                    {columns.map((column) => (
+                        <th key={column.key} style={{ width: column.width }} className={styles["table__cell"]}>
+                            {column.headerRender ? column.headerRender() : column.label}
+                        </th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {data.length === 0 ? <tr><td colSpan={columns.length} className={styles["table__cell"]} style={{ textAlign: 'center' }}> No data found.</td></tr> : (
+                    data.map((row, rowIndex) => (
+                        <tr key={rowIndex} className={styles["table__row"]}>
+                            {columns.map((column) => (
+                                <td key={column.key} className={styles["table__cell"]}>
+                                    {column.render ? column.render(row) : row[column.key]}
+                                </td>
+                            ))}
+                        </tr>
+                    ))
+                )}
+            </tbody>
+        </table>
+    )
+}
+
+export default Table
