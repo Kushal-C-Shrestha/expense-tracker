c431ef5b8650672f6fc2c0c9c63058ef6cf0bf9c
Kushal Chandra Shrestha <kushal.c.shrestha@gmail.com>
2026-04-24 15:34:16 +0545

    refactor: Used browser router for home page and moved the code to home page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
 import { useState, useEffect } from "react"
+import Home from "./pages/Home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
-import AddExpense from "./components/AddExpense"
-import Search from "./components/Search"
-import ExpenseList from "./components/ExpenseList"
-import Filter from "./components/Filter"
 import "./App.css"
 
 function App() {
-  const [expenses, setExpenses] = useState([]);
-  const [isFiltering, setIsFiltering] = useState(false);
-  const [filteredExpenses, setFilteredExpenses] = useState([]);
-  const filters = ["Food", "Transportation", "Entertainment"]
-
-  useEffect(() => { console.log(expenses) }, [expenses])
   return (
     <>
-      <h1>Expense tracker</h1>
-      <div className="search-filter-container">
-        <Search expenses={expenses} setFilteredExpenses={setFilteredExpenses} setIsFiltering={setIsFiltering} />
-        <Filter filters={filters} setFilteredExpenses={setFilteredExpenses} setIsFiltering={setIsFiltering} expenses={expenses} />
-      </div>
-      <AddExpense setExpenses={setExpenses} filters={filters} />
-      <ExpenseList expenses={expenses} filteredExpenses={filteredExpenses} isFiltering={isFiltering} setExpenses={setExpenses} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
+
 
 export default App
