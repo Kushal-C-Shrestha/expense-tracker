82a94b605e0ab83c051a8ed8308685e8641d2b81
Kushal Chandra Shrestha <kushal.c.shrestha@gmail.com>
2026-04-27 16:54:31 +0545

    Created sorting of expenses, button for clearing filters, moved app.css code to home.css, created variables for index.css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import ExpenseForm from "../../components/ExpenseForm/ExpenseForm"
 import ExpenseList from "../../components/ExpenseList/ExpenseList"
 import Button from '../../components/ui/Button/Button'
-import { Plus, Search } from 'lucide-react'
+import { Plus, Search, X } from 'lucide-react'
 import Input from '../../components/ui/Input/Input'
 import Select from '../../components/ui/Select/Select'
 import styles from "./Home.module.css"
@@ -53,18 +53,45 @@ const Home = () => {
         setSelectedIds([]);
     }
 
+    const handleSort = (e) => {
+        console.log(e.target.value);
+        const expensesToSort = isFiltering ? filteredExpenses : expenses;
+        console.log(expensesToSort);
+        const selectedSort = e.target.value;
+        e.target.value = selectedSort;
+        let sortedExpenses = [];
+        if (selectedSort === "Sort by Date: Ascending") {
+            sortedExpenses = [...expensesToSort].sort((a, b) => new Date(a.date) - new Date(b.date));
+        } else if (selectedSort === "Sort by Date: Descending") {
+            sortedExpenses = [...expensesToSort].sort((a, b) => new Date(b.date) - new Date(a.date));
+        } else if (selectedSort === "Sort by Amount: (Low to high)") {
+            sortedExpenses = [...expensesToSort].sort((a, b) => a.amount - b.amount);
+        } else if (selectedSort === "Sort by Amount: (High to low)") {
+            sortedExpenses = [...expensesToSort].sort((a, b) => b.amount - a.amount);
+        }
+        console.log(sortedExpenses);
+        setFilteredExpenses(sortedExpenses);
+        setIsFiltering(true);
+    };
+
+    const handleClearFilters = () => {
+        setIsFiltering(false);
+        setFilteredExpenses([]);
+    }
 
 
     return (
         <>
-            <h1>Welcome</h1>
-            <div className="container">
-                <div className="container__header">
-                    <h3 className='container__title'>Expense tracker</h3>
-                    <div className="container__controls">
-                        <Input placeholder="Search expenses" onChange={handleSearch} rightIcon={<Search />} variant="search" />
+            <div className={styles["container"]}>
+                <div className={styles["container__header"]}>
+                    <div className={styles["container__header--left"]}>
+                        <Input placeholder="Search expenses" onChange={handleSearch} rightIcon={<Search color='#bababa' strokeWidth={1.5} />} variant="search" />
                         <Select options={["All", ...filters]} onChange={handleFilter} />
-                        <Button text="Add Expense" icon={<Plus />} onClick={() => setIsOpenModal(true)} variant='add' />
+                        <Select options={["Sort by Date: Ascending", "Sort by Date: Descending", "Sort by Amount: (Low to high)", "Sort by Amount: (High to low)"]} onChange={handleSort} />
+                        {isFiltering && <Button text="Clear Filters" onClick={handleClearFilters} variant='clear' icon={<X/>} />}
+                    </div>
+                    <div className={styles["container__header--right"]}>
+                        <Button text="Add Expense" icon={<Plus size={18} />} onClick={() => setIsOpenModal(true)} variant='add' />
                     </div>
                 </div>
                 <ExpenseList expenses={expenses} filteredExpenses={filteredExpenses} isFiltering={isFiltering} setExpenses={setExpenses} setSelectedExpense={setSelectedExpense} handleOpenModal={handleOpenModal} selectedIds={selectedIds} setSelectedIds={setSelectedIds} />
