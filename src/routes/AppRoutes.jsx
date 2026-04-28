4ad26d6cf1c14ca81ea0c7511801ce3eec1bb680
Kushal Chandra Shrestha <kushal.c.shrestha@gmail.com>
2026-04-28 17:18:12 +0545

    Created and integrated dashboard.

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import {  Expenses } from "../pages/index"
+import { Dashboard, Expenses } from "../pages/index"
 import DashboardLayout from '../Layouts/DashboardLayout'
 
 const AppRoutes = () => {
@@ -8,6 +8,7 @@ const AppRoutes = () => {
         <Router>
             <Routes>
                 <Route element={<DashboardLayout />}>
+                    <Route path="/" element={<Dashboard />} />
                     <Route path="/expenses" element={<Expenses />} />
                 </Route>
             </Routes>
