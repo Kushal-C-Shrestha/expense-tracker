33e84e4285b9aeacbaa9240b92e5f46e018b0cbb
Kushal Chandra Shrestha <kushal.c.shrestha@gmail.com>
2026-04-28 17:39:28 +0545

    fix: Updated schema to fix the expense not adding bug.

--- src/schemas/expense.schema.js
+++ src/schemas/expense.schema.js
@@ -4,5 +4,5 @@ export const expenseSchema = z.object({
     title: z.string().min(1, "Title is required"),
     amount: z.string().min(1, "Amount is required").refine(value => !isNaN(parseFloat(value)) && parseFloat(value) > 0, "Amount must be a positive number"),
     date: z.string().min(1, "Date is required").refine(date => !isNaN(Date.parse(date)), "Invalid date format"),
-    category: z.enum(["Food", "Transportation", "Entertainment"])
+    category: z.enum(["Food", "Transportation", "Entertainment", "Utilities", "Healthcare", "Other"]),
 })
