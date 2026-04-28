7479b3a9a3afd68a5f59cda6b0fa9508c014f406
Kushal Chandra Shrestha <kushal.c.shrestha@gmail.com>
2026-04-28 13:53:58 +0545

    Used modal component for expense form.

--- src/components/ExpenseForm/ExpenseForm.jsx
+++ src/components/ExpenseForm/ExpenseForm.jsx
@@ -9,6 +9,7 @@ import Select from "../ui/Select/Select";
 import Button from "../ui/Button/Button";
 import InputGroup from "../InputGroup";
 import styles from "./ExpenseForm.module.css"
+import Modal from "../ui/Modal/Modal";
 
 import filters from "../../data/filters";
 
@@ -62,33 +63,31 @@ function ExpenseForm({ setExpenses, handleCloseModal, filters, selectedExpense =
 
     return (
         <>
-            <form onSubmit={handleSubmit(onSubmit)} className={styles["expense__form"]} onClick={(e) => e.stopPropagation()}>
-                <div className={styles["form__header"]}>
-                    <h2 className={styles["form__title"]}>Add Expense</h2>
-                    <X onClick={handleClose} />
-                </div>
-                <div className={styles["form__row"]}>
-                    <InputGroup label="Title" errors={errors} name="title" className={styles["form__group"]}>
-                        <Input   {...register("title")} placeholder="Enter expense title" />
-                    </InputGroup>
-                    <InputGroup label="Amount" errors={errors} type="number" step="0.01" name="amount" className={styles["form__group"]} >
-                        <Input type="number" step="0.01"  {...register("amount")} placeholder="Enter amount" />
-                    </InputGroup>
-                </div>
+            <form onSubmit={handleSubmit(onSubmit)} onClick={(e) => e.stopPropagation()}>
+                <Modal title={selectedExpense ? "Edit Expense" : "Add Expense"} handleClose={handleClose} footer={() => {
+                    return (<>
+                        <Button text="Cancel" type="button" onClick={handleClose} variant="cancel" icon={<X />} />
+                        <Button text="Save" type="submit" icon={<Check />} variant="save" disabled={!title || !amount || !date || !category} />
+                    </>)
+                }} >
+                    <div className={styles["form__row"]}>
+                        <InputGroup label="Title" errors={errors} name="title" className={styles["form__group"]}>
+                            <Input   {...register("title")} placeholder="Enter expense title" />
+                        </InputGroup>
+                        <InputGroup label="Amount" errors={errors} type="number" step="0.01" name="amount" className={styles["form__group"]} >
+                            <Input type="number" step="0.01"  {...register("amount")} placeholder="Enter amount" />
+                        </InputGroup>
+                    </div>
 
-                <div className={styles["form__row"]}>
-                    <InputGroup label="Date" errors={errors} type="date" name="date" className={styles["form__group"]}>
-                        <Input type="date" {...register("date")} />
-                    </InputGroup>
-                    <InputGroup label="Category" errors={errors} name="category" className={styles["form__group"]}>
-                        <Select options={filters}  {...register("category")} />
-                    </InputGroup>
-                </div>
-
-                <div className={styles["form__actions"]}>
-                    <Button text="Cancel" type="button" onClick={handleClose} variant="cancel" icon={<X />} />
-                    <Button text="Save" type="submit" icon={<Check />} variant="save" disabled={!title || !amount || !date || !category} />
-                </div>
+                    <div className={styles["form__row"]}>
+                        <InputGroup label="Date" errors={errors} type="date" name="date" className={styles["form__group"]}>
+                            <Input type="date" {...register("date")} />
+                        </InputGroup>
+                        <InputGroup label="Category" errors={errors} name="category" className={styles["form__group"]}>
+                            <Select options={filters}  {...register("category")} />
+                        </InputGroup>
+                    </div>
+                </Modal>
             </form>
         </>
     )
